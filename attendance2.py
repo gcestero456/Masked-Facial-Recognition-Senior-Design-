6e0f0e254ec171ae3c89acc6fa7e048ee550d73b
gcestero456 <64175797+gcestero456@users.noreply.github.com>
2021-12-07 11:43:43 -0500

    Update attendance2.py

--- attendance2.py
+++ attendance2.py
@@ -24,23 +24,23 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
-#def createAttendance(name):
+def createAttendance(name):
     #datetime library usage
- #   with open('Attendance.csv', 'r+ ') as f:
-  #      myDataList = f.readlines()
-   #     nameList = []
-    #    for line in myDataList:
-     #       entry = line.split(',')
-      #      nameList.append(entry[0])
-       # if name not in nameList:
-        #    now = datetime.now()
-         #   dateString = now.strftime('%H:%M:%S')
-          #  f.writelines(f'\n{name},{dateString}')
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dateString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dateString}')
 
-#createAttendance('a')
 
 
 encodeListKnown = findEncodings(images)
+print('Encoding Complete')
 
 cap = cv2.VideoCapture(0)
 
@@ -74,11 +74,11 @@ while True:
                 cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 0, 0), cv2.FILLED)
                 cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
                 cv2.imshow('Webcam', img)
-                #createAttendance(name)
+                createAttendance(name)
 
         #press'esc' to close program
-        if cv2.waitKey(1) == 27:
-            break
+        cv2.imshow('Webcam',img)
+        cv2.waitKey(1)
 #release camera
 cap.release()
 cv2.destroyAllWindows()
